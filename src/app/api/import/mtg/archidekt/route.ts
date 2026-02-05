174194f75bb6e867b0a0f9a44bcba90fe80d84e6
mm4096 <minecraftmath4096@gmail.com>
2026-02-06 08:32:24 +1300

    archidekt imports

---
+++ src/app/api/import/mtg/archidekt/route.ts
@@ -0,0 +1,52 @@
+import {NextRequest} from "next/server";
+
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+	const searchParams = request.nextUrl.searchParams;
+	if (!searchParams.has("url") || searchParams.get("url") == null) {
+		return new Response(JSON.stringify({
+			message: "Missing Archidekt URL.",
+		}), {status: 400})
+	}
+
+	const archidektUrl = searchParams.get("url")!;
+	const url: URL = new URL(archidektUrl);
+	const deckId = url.pathname.split("/")[2];
+	if (deckId == null || isNaN(parseInt(deckId))) {
+		return new Response(JSON.stringify({
+			message: "Invalid Archidekt URL.",
+		}), {status: 400})
+	}
+
+	const archidecktResponse = await fetch(`https://archidekt.com/api/decks/${deckId}/`);
+	if (!archidecktResponse.ok) {
+		if (archidecktResponse.status === 404) {
+			return new Response(JSON.stringify({
+				message: "Deck not found. Please make sure your deck is set to \"Public\" or \"Unlisted\", and the URL is correct.",
+			}), {status: 404})
+		}
+		return new Response(JSON.stringify({
+			message: `Error fetching deck: ${archidecktResponse.statusText}`,
+		}), {status: 500});
+	}
+
+	const deck = await archidecktResponse.json();
+	const cards = deck["cards"];
+
+	let import_cards: string[] = [];
+	for (const card of cards) {
+		const quantity = card["quantity"];
+		const name = card["card"]["oracleCard"]["name"];
+		import_cards.push(`${quantity} ${name}`);
+	}
+	const card_list = import_cards.join("\n");
+	console.log(card_list);
+	body["cards"] = card_list;
+
+
+	const origin = request.nextUrl.origin;
+	return fetch(`${origin}/api/import/mtg`, {
+		method: "POST",
+		body: JSON.stringify(body)
+	});
+}
--- src/app/editor/mtg/components/import.tsx
+++ src/app/editor/mtg/components/import.tsx
@@ -3,7 +3,6 @@
 import {useRef, useState} from "react";
 import {MTGCard} from "@/lib/card";
 import {ReminderTextBehavior} from "@/lib/mtg";
-import {ExperimentalBadge} from "@/app/components/tags/experimental";
 import {BiInfoCircle} from "react-icons/bi";
 import {useUmamiEvent} from "@/app/components/analytics";
 import {NewBadge} from "@/app/components/tags/new";
@@ -26,6 +25,8 @@ export function ImportMTG({cards, setCardsAction}: {
 	const [importIncludeTokens, setImportIncludeTokens] = useState<boolean>(false);
 	const [importSplitDFCs, setImportSplitDFCs] = useState<boolean>(false);
 
+	const [importType, setImportType] = useState<"archidekt" | "list">("list");
+
 	const umamiTracker = useUmamiEvent();
 
 	async function importCards() {
@@ -33,7 +34,16 @@ export function ImportMTG({cards, setCardsAction}: {
 		setImportMessage("Fetching Cards...");
 		setImportError("");
 
-		fetch("/api/import/mtg", {
+		let fetchUrl: string;
+		switch (importType) {
+			case "archidekt":
+				fetchUrl = `/api/import/mtg/archidekt?url=${encodeURIComponent(importText)}`;
+				break;
+			case "list":
+				fetchUrl = `/api/import/mtg`;
+		}
+
+		fetch(fetchUrl, {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
@@ -53,8 +63,7 @@ export function ImportMTG({cards, setCardsAction}: {
 
 				if (overwrite) {
 					setCardsAction(retCards);
-				}
-				else {
+				} else {
 					const newCards: MTGCard[] = [];
 					newCards.push(...cards);
 					newCards.push(...retCards);
@@ -67,7 +76,7 @@ export function ImportMTG({cards, setCardsAction}: {
 				setImportError(json["message"]);
 			}
 
-			umamiTracker("mtg-CardsImported", {success: response.ok});
+			umamiTracker("mtg-CardsImported", {importType: importType, success: response.ok});
 
 		}).catch((e) => {
 			console.log(e);
@@ -86,52 +95,90 @@ export function ImportMTG({cards, setCardsAction}: {
 		</button>
 		<dialog className="modal w-full" ref={dialogRef}>
 			<div className="modal-box min-w-[50vw] max-w-[100vw] md:max-w-[75vw] max-h-[90vh] overflow-y-auto">
-				<h3 className="font-bold text-xl">Import Cards</h3>
-
+				<h2>Import Cards</h2>
 				<div className="custom-divider"/>
 
-				<p>Paste a list of cards below. Card names must be exact (except symbols and capitalization) and must
-					match one of the following formats:</p>
-				<div className="flex flex-col md:flex-row w-full">
-					<div className="border p-2 grow md:w-max">
-						<p>Plains</p>
-						<p>Deflecting Swat</p>
-						<p>Deflecting Swat</p>
-						<p>sakura tribe elder</p>
-						<p>chandra flames fury</p>
-						<p>commit // memory</p>
-					</div>
-					<div className="border p-2 grow md:w-max">
-						<p>2 Plains</p>
-						<p>2 Deflecting Swat</p>
-						<p>4 sakura tribe elder</p>
-						<p>10 chandra flames fury</p>
-						<p>1 Commit</p>
-					</div>
-					<div className="border p-2 grow md:w-max">
-						<p>2x Plains</p>
-						<p>2x Deflecting Swat</p>
-						<p>4x sakura tribe elder</p>
-						<p>10x chandra flames fury</p>
-						<p>1x Memory</p>
-					</div>
-				</div>
-				<p className="text-xs">All cards must either have no quantity given, or all cards must have quantities.<br/>
-				Headers (such as &quot;Main Deck&quot; or &quot;Sideboard&quot;) MUST be removed.</p>
+				<div className="tabs tabs-border">
 
-				<fieldset className="fieldset">
-					<legend className="fieldset-legend"></legend>
-					<textarea className="textarea w-full" placeholder="Paste your card data here" value={importText}
-							  onChange={(e) => {
-								  setImportText(e.target.value);
-							  }}/>
-					{
-						importMessage !== "" && (<label className="label">{importMessage}</label>)
-					}
-					{
-						importError !== "" && (<label className="label text-error whitespace-pre">{importError}</label>)
-					}
-				</fieldset>
+					<label className="tab">
+						<input type="radio" name="mtg-import-type"
+							   id="list-import"
+							   defaultChecked={true}
+							   onChange={() => {
+								   setImportType("list");
+							   }}/>
+
+						<span>Import from List</span>
+					</label>
+					<div className="tab-content border-black p-3">
+						<p>Paste a list of cards below. Card names must be exact (except symbols and capitalization) and
+							must
+							match one of the following formats:</p>
+						<div className="flex flex-col md:flex-row w-full">
+							<div className="border p-2 grow md:w-max">
+								<p>Plains</p>
+								<p>Deflecting Swat</p>
+								<p>Deflecting Swat</p>
+								<p>sakura tribe elder</p>
+								<p>chandra flames fury</p>
+								<p>commit // memory</p>
+							</div>
+							<div className="border p-2 grow md:w-max">
+								<p>2 Plains</p>
+								<p>2 Deflecting Swat</p>
+								<p>4 sakura tribe elder</p>
+								<p>10 chandra flames fury</p>
+								<p>1 Commit</p>
+							</div>
+							<div className="border p-2 grow md:w-max">
+								<p>2x Plains</p>
+								<p>2x Deflecting Swat</p>
+								<p>4x sakura tribe elder</p>
+								<p>10x chandra flames fury</p>
+								<p>1x Memory</p>
+							</div>
+						</div>
+						<p className="text-xs">All cards must either have no quantity given, or all cards must have
+							quantities.<br/>
+							Headers (such as &quot;Main Deck&quot; or &quot;Sideboard&quot;) MUST be removed.</p>
+						<fieldset className="fieldset">
+							<legend className="fieldset-legend"></legend>
+							<textarea className="textarea w-full" placeholder="Paste your card data here"
+									  value={importText}
+									  onChange={(e) => {
+										  setImportText(e.target.value);
+									  }}/>
+						</fieldset>
+					</div>
+
+
+					<label className="tab">
+						<input type="radio" name="mtg-import-type"
+							   id="archidekt-import"
+							   onChange={() => {
+								   setImportType("archidekt");
+							   }}/>
+
+						<span>Import from Archidekt <NewBadge/></span>
+					</label>
+					<div className="tab-content border-black p-3">
+						<p>Paste in your Archidekt deck URL here:</p>
+						<input className="input w-full" type="url"
+							   placeholder="https://archidekt.com/decks/1234567890/my-first-deck" value={importText}
+							   onChange={(e) => {
+								   setImportText(e.target.value);
+							   }}/>
+					</div>
+
+				</div>
+
+
+				{
+					importMessage !== "" && (<label className="label">{importMessage}</label>)
+				}
+				{
+					importError !== "" && (<label className="label text-error whitespace-pre">{importError}</label>)
+				}
 				<br/>
 
 				<div className="collapse bg-base-100 border-gray-500 border h-max">
@@ -139,12 +186,14 @@ export function ImportMTG({cards, setCardsAction}: {
 					<div className="collapse-title font-semibold pr-8">Additional Settings</div>
 					<div className="collapse-content flex flex-col md:flex-row overflow-x-none flex-wrap">
 						<label className="label text-sm">
-							<input type="checkbox" className="checkbox checkbox-sm" checked={importBasicLands} onChange={(e) => {
-								setImportBasicLands(e.target.checked);
-							}}/>
+							<input type="checkbox" className="checkbox checkbox-sm" checked={importBasicLands}
+								   onChange={(e) => {
+									   setImportBasicLands(e.target.checked);
+								   }}/>
 							Import basic lands
 							<span className="tooltip tooltip-right ">
-								<span className="tooltip-content">If unchecked, any card who&apos;s name is exactly &quot;Plains&quot;, &quot;Mountain&quot;, &quot;Swamp&quot;, &quot;Forest&quot;, or &quot;Island&quot; will be skipped.</span>
+								<span
+									className="tooltip-content">If unchecked, any card who&apos;s name is exactly &quot;Plains&quot;, &quot;Mountain&quot;, &quot;Swamp&quot;, &quot;Forest&quot;, or &quot;Island&quot; will be skipped.</span>
 								<BiInfoCircle/>
 							</span>
 						</label>
@@ -152,9 +201,10 @@ export function ImportMTG({cards, setCardsAction}: {
 						<div className="divider md:divider-horizontal"/>
 
 						<label className="label text-sm">
-							<select className="select select-sm w-min" value={importReminderTextBehavior} onChange={(e) => {
-								setImportReminderTextBehavior(parseInt(e.target.value) as ReminderTextBehavior);
-							}}>
+							<select className="select select-sm w-min" value={importReminderTextBehavior}
+									onChange={(e) => {
+										setImportReminderTextBehavior(parseInt(e.target.value) as ReminderTextBehavior);
+									}}>
 								<option value={ReminderTextBehavior.NORMAL}>Render reminder text as normal text</option>
 								<option value={ReminderTextBehavior.ITALIC}>Italicize reminder text</option>
 								<option value={ReminderTextBehavior.HIDDEN}>Exclude reminder text</option>
@@ -168,9 +218,10 @@ export function ImportMTG({cards, setCardsAction}: {
 						<div className="divider md:divider-horizontal"/>
 
 						<label className="label text-sm">
-							<input type="checkbox" className="checkbox checkbox-sm" checked={importSplitDFCs} onChange={(e) => {
-								setImportSplitDFCs(e.target.checked);
-							}}/>
+							<input type="checkbox" className="checkbox checkbox-sm" checked={importSplitDFCs}
+								   onChange={(e) => {
+									   setImportSplitDFCs(e.target.checked);
+								   }}/>
 							Split DFCs into separate cards
 							<span className="tooltip tooltip-right ">
 								<span className="tooltip-content">If checked, all DFCs will be imported as two cards instead of one.</span>
@@ -181,9 +232,10 @@ export function ImportMTG({cards, setCardsAction}: {
 						<div className="divider md:divider-horizontal"/>
 
 						<label className="label text-sm">
-							<input type="checkbox" className="checkbox checkbox-sm" checked={importTemplates} onChange={(e) => {
-								setImportTemplates(e.target.checked);
-							}}/>
+							<input type="checkbox" className="checkbox checkbox-sm" checked={importTemplates}
+								   onChange={(e) => {
+									   setImportTemplates(e.target.checked);
+								   }}/>
 							Automatically apply templates
 							<span className="tooltip tooltip-left ">
 								<span className="tooltip-content">Whether to automatically apply templates based on card types (such as the Planeswalker template for Planeswalkers or Spacecraft template for Spacecraft)</span>
